b18ee6bd403cc86ae7022aa04dcb648557404045
Bob Tanner <tanner@real-time.com>
2018-05-10 21:54:48 -0500

    Bring package specs to current standards.
    Remove the follow warning:
    
        This package was created prior to July 2014 - when package content started
        being included on the site. You can download and unzip the package or use
        Nuget Package Explorer to see the contents.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,31 @@
-$package = 'SQL2008R2.NativeClient'
+$ErrorActionPreference = 'Stop'; 
 
-try {
-  $params = @{
-    packageName = $package;
-    fileType = 'msi';
-    silentArgs = ' /qb IACCEPTSQLNCLILICENSETERMS=YES';
-    url = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/sqlncli.ms';
-    url64bit = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/sqlncli.msi';
-  }
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/sqlncli.msi';
+$checksum   = '012aca6cef50ed784f239d1ed5f6923b741d8530b70d14e9abcb3c7299a826cc'
 
-  Install-ChocolateyPackage @params
-
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
+
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+#
+Install-ChocolateyZipPackage @packageArgs 
+
+  # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
+  # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
+  #
+  $installArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    file           = Join-Path $toolsDir 'sqlncli.msi'
+    fileType       = 'msi'
+    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`" ACCEPTSQLNCLILICENSETERMS=YES" 
+    validExitCodes = @(0, 3010, 1641)
+  }
+  
+  # https://chocolatey.org/docs/helpers-install-chocolatey-package
+  #
+  Install-ChocolateyInstallPackage @installArgs
