3f518b48dc06e7b8b6e6baa02a309557016c16a4
Bob Tanner <tanner@real-time.com>
2018-05-11 14:34:20 +0100

    Switch silent option to /qb for basic UI
    The switch is IACCEPTSQLNCLILICENSETERMS (added the I)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,31 +1,20 @@
 $ErrorActionPreference = 'Stop'; 
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/sqlncli.msi';
+$url        = 'https://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/sqlncli.msi';
 $checksum   = '012aca6cef50ed784f239d1ed5f6923b741d8530b70d14e9abcb3c7299a826cc'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  url            = $url
+  silentArgs     = "/qb /norestart /l*v `"$($env:TEMP)\$($packageName).log`" IACCEPTSQLNCLILICENSETERMS=YES" 
+  validExitCodes = @(0, 3010, 1641)
   checksum       = $checksum
   checksumType   = 'sha256' 
 }
 
-# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+# https://chocolatey.org/docs/helpers-install-chocolatey-package
 #
-Install-ChocolateyZipPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
 
-  # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
-  # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
-  #
-  $installArgs = @{
-    packageName    = $env:ChocolateyPackageName
-    file           = Join-Path $toolsDir 'sqlncli.msi'
-    fileType       = 'msi'
-    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`" ACCEPTSQLNCLILICENSETERMS=YES" 
-    validExitCodes = @(0, 3010, 1641)
-  }
-  
-  # https://chocolatey.org/docs/helpers-install-chocolatey-package
-  #
-  Install-ChocolateyInstallPackage @installArgs
