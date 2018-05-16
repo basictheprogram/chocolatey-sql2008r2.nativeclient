4b30ce28e477ded0403f3b7fde17d61cd97e5649
Bob Tanner <tanner@real-time.com>
2018-05-16 18:14:23 -0500

    Fixes: Guidelines are strong suggestions that improve the quality of a package version. These are considered something to fix for next time to increase the quality of the package. Over time Guidelines can become Requirements. A package version can be approved without addressing Guideline comments but will reduce the quality of the package.
    Write-ChocolateySuccess/Write-ChocolateyFailure have been deprecated, but have been found in the package's automation scripts. Remove the usage of those two functions. If there is an error, throw the error. More... This can also give a false positive for commented code with the words:
    
    Write-ChocolateySuccess
    
    Write-ChocolateyFailure

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,16 +1,32 @@
+$ErrorActionPreference = 'Stop';
+
+# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
 # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-$msiId = ' {4AB6A079-178B-4144-B21F-4D1AE71666A2}'
-$msiId64 = '{2180B33F-3225-423E-BBC1-7798CFD3CD1F}'
+#
+$msiId       = '{4AB6A079-178B-4144-B21F-4D1AE71666A2}'
+$msiId64     = '{2180B33F-3225-423E-BBC1-7798CFD3CD1F}'
+$packageName = 'SQL2008R2.NativeClient'
 
-$package = 'SQL2008R2.NativeClient'
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'SQL Server 2008 R2 Native Client*'
 
-try {
-  
-  if ([IntPtr]::Size -eq 8) { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId64 /qb" -validExitCodes @(0) }
-  else { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId /qb" -validExitCodes @(0) }
-  # the following is all part of error handling
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw 
+if ($key.Count -eq 1) {
+  $key | ForEach-Object {
+  $packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'MSI'
+    silentArgs     = '/qb'
+    validExitCodes = @(0)
+    file           = "$($_.UninstallString)"
+  }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
