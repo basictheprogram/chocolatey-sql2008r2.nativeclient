fd2f70d399d4a7e78aa62e7ef602a7a8be9aefea
Bob Tanner <tanner@real-time.com>
2020-05-20 02:04:40 -0500

    Fix automatic package installation testing failure
        Fixed the sha256 checksum on x86 package
        Fixed the sha256 checksum on x64 package
    
        Change end of line to LF
    
        Deleted boilerplate ReadMe.md

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'; 
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x86/sqlncli.msi'
-$checksum   = 'a9c754af47193bc836dc1e2c88b56177812320dccca5c85c204f00bee5780aee'
-$url64bit   = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x64/sqlncli.msi'
-$checksum64 = '4296bbebd733db708ee53c3753e8c0d1208faf4d5ee8dcd1f2f149c986022994'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x86/sqlncli.msi'
+$checksum = 'f01c4d4df7caebd0dd164b7a7ed978282535f3951cea523661f0d043c6775ca5'
+$url64bit = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x64/sqlncli.msi'
+$checksum64 = '8222bebd63c2c79bdf0bfbcb614d43806799891d9348ff8da7df8f458a7f8bc6'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
