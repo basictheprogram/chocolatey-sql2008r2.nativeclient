2ff787d4302df0162750bc8de1b476d7a0e52d93
Bob Tanner <tanner@real-time.com>
2021-06-28 11:34:09 -0500

    WIP: https://support.real-time.com/support/Ticket/Display.html?id=77151

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'; 
+$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x86/sqlncli.msi'
@@ -7,16 +7,16 @@ $url64bit = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B9
 $checksum64 = '8222bebd63c2c79bdf0bfbcb614d43806799891d9348ff8da7df8f458a7f8bc6'
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'msi'
-  url            = $url
-  checksum       = $checksum
-  checksumType   = 'sha256' 
-  url64bit       = $url64bit
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'
-  silentArgs     = "/qb /norestart /l*v `"$($env:TEMP)\$($packageName).log`" IACCEPTSQLNCLILICENSETERMS=YES" 
-  validExitCodes = @(0, 3010, 1641)
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'msi'
+    url            = $url
+    checksum       = $checksum
+    checksumType   = 'sha256'
+    url64bit       = $url64bit
+    checksum64     = $checksum64
+    checksumType64 = 'sha256'
+    silentArgs     = "/qb /norestart /l*v `"$($env:TEMP)\$($packageName).log`" IACCEPTSQLNCLILICENSETERMS=YES"
+    validExitCodes = @(0, 3010, 1641)
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-package
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -3,30 +3,32 @@ $ErrorActionPreference = 'Stop';
 # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
 # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
 #
-$msiId       = '{4AB6A079-178B-4144-B21F-4D1AE71666A2}'
-$msiId64     = '{2180B33F-3225-423E-BBC1-7798CFD3CD1F}'
+$msiId = '{4AB6A079-178B-4144-B21F-4D1AE71666A2}'
+$msiId64 = '{2180B33F-3225-423E-BBC1-7798CFD3CD1F}'
 $packageName = 'SQL2008R2.NativeClient'
 
 $uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName 'SQL Server 2008 R2 Native Client*'
 
 if ($key.Count -eq 1) {
-  $key | ForEach-Object {
-  $packageArgs = @{
-    packageName    = $packageName
-    fileType       = 'MSI'
-    silentArgs     = '/qb'
-    validExitCodes = @(0)
-    file           = "$($_.UninstallString)"
-  }
+    $key | ForEach-Object {
+        $packageArgs = @{
+            packageName    = $packageName
+            fileType       = 'MSI'
+            silentArgs     = '/qb'
+            validExitCodes = @(0)
+            file           = "$($_.UninstallString)"
+        }
 
-    Uninstall-ChocolateyPackage @packageArgs
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$($key.Count) matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
 }
